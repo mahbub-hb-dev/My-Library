5660491c6036cea681a1a22271a3f56ed9f9e698
Mahbub Hasan <mahbub.hb.dev@gmail.com>
2025-11-09 14:36:51 +0600

    Data of popular classic books has been added

--- src/components/ClassicBooks.jsx
+++ src/components/ClassicBooks.jsx
@@ -1,4 +1,77 @@
-import React from 'react'
+const popularClassicBooks = [
+    {
+        id : 1,
+        title : "Excercise Is Murder",
+        image : "/images/classics/classic-1.jpg",
+        alt : "classic book 1",
+    },
+    {
+        id : 2,
+        title : "Sister Witches",
+        image : "/images/classics/classic-2.jpg",
+        alt : "classic book 2",
+    },
+    {
+        id : 3,
+        title : "From Darkness to Light",
+        image : "/images/classics/classic-3.jpg",
+        alt : "classic book 3",
+    },
+    {
+        id : 4,
+        title : "The Tunnels Of Rasand",
+        image : "/images/classics/classic-4.jpg",
+        alt : "classic book 4",
+    },
+    {
+        id : 5,
+        title : "Creature Of Ancient Dreaming",
+        image : "/images/classics/classic-5.jpg",
+        alt : "classic book 5",
+    },
+    {
+        id : 6,
+        title : "Creature Of Ancient Dreaming",
+        image : "/images/classics/classic-6.jpg",
+        alt : "classic book 6",
+    },
+    {
+        id : 7,
+        title : "Excercise Is Murder",
+        image : "/images/classics/classic-1.jpg",
+        alt : "classic book 1",
+    },
+    {
+        id : 8,
+        title : "Sister Witches",
+        image : "/images/classics/classic-2.jpg",
+        alt : "classic book 2",
+    },
+    {
+        id : 9,
+        title : "From Darkness to Light",
+        image : "/images/classics/classic-3.jpg",
+        alt : "classic book 3",
+    },
+    {
+        id : 10,
+        title : "The Tunnels Of Rasand",
+        image : "/images/classics/classic-4.jpg",
+        alt : "classic book 4",
+    },
+    {
+        id : 11,
+        title : "Creature Of Ancient Dreaming",
+        image : "/images/classics/classic-5.jpg",
+        alt : "classic book 5",
+    },
+    {
+        id : 12,
+        title : "Creature Of Ancient Dreaming",
+        image : "/images/classics/classic-6.jpg",
+        alt : "classic book 6",
+    },
+]
 
 const ClassicBooks = () => {
   return (
@@ -7,65 +80,14 @@ const ClassicBooks = () => {
         <h2 className='common-h2'> POPULAR CLASSICS &nbsp; <a href="#"> (View All) </a> </h2>
 
         <div className='common-div justify-center grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-            <div>
-                <img src="/images/classics/classic-1.jpg" alt="classic book 1" />
-                <p> <a href="#" className='hover:underline'> Excercise Is Murder</a> </p>
-            </div>
 
-            <div>
-                <img src="/images/classics/classic-2.jpg" alt="classic book 2" />
-                <p> <a href="#" className='hover:underline'> Sister Witches </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-3.jpg" alt="classic book 3" />
-                <p> <a href="#" className='hover:underline'> From Darkness to Light </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-4.jpg" alt="classic book 4" />
-                <p> <a href="#" className='hover:underline'> The Tunnels Of Rasand </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-5.jpg" alt="classic book 5" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-6.jpg" alt="classic book 6" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-7.jpg" alt="classic book 7" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-8.jpg" alt="classic book 8" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-9.jpg" alt="classic book 9" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-10.jpg" alt="classic book 10" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-11.jpg" alt="classic book 11" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/classics/classic-12.jpg" alt="classic book 12" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
+            {popularClassicBooks.map((data, index) => (
+                <div key={index}>
+                    <img src={data.image} alt={data.alt} />
+                    <p> <a href="#" className='hover:underline'> {data.title} </a> </p>
+                </div>
+            ))}
+            
         </div>
 
     </div>
--- src/components/TrendingBooks.jsx
+++ src/components/TrendingBooks.jsx
@@ -1,4 +1,35 @@
-import React from 'react'
+const trendingBooksData = [
+    {
+        id : 1,
+        title : "Excercise Is Murder",
+        image : "/images/trending/t-1.jpg",
+        alt : "genre 1"
+    },
+    {
+        id : 2,
+        title : "Sister Witches",
+        image : "/images/trending/t-2.jpg",
+        alt : "genre 2"
+    },
+    {
+        id : 3,
+        title : "From Darkness to Light",
+        image : "/images/trending/t-3.jpg",
+        alt : "genre 3"
+    },
+    {
+        id : 4,
+        title : "The Tunnels Of Rasand",
+        image : "/images/trending/t-4.jpg",
+        alt : "genre 4"
+    },
+    {
+        id : 5,
+        title : "Creature Of Ancient Dreaming",
+        image : "/images/trending/t-5.jpg",
+        alt : "genre 5"
+    },
+]
 
 const TrendingBooks = () => {
   return (
@@ -7,30 +38,12 @@ const TrendingBooks = () => {
         <h2 className='common-h2'> TRENDING BOOK'S &nbsp; <a href="#"> (View All) </a> </h2>
 
         <div className='justify-center gap-3 max-sm:grid max-sm:grid-cols-2 min-sm:flex min-sm:flex-wrap'>
-            <div>
-                <img src="/images/trending/t-1.jpg" alt="genre 1" />
-                <p> <a href="#" className='hover:underline'> Excercise Is Murder</a> </p>
-            </div>
-
-            <div>
-                <img src="/images/trending/t-2.jpg" alt="genre 2" />
-                <p> <a href="#" className='hover:underline'> Sister Witches </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/trending/t-3.jpg" alt="genre 3" />
-                <p> <a href="#" className='hover:underline'> From Darkness to Light </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/trending/t-4.jpg" alt="genre 4" />
-                <p> <a href="#" className='hover:underline'> The Tunnels Of Rasand </a> </p>
-            </div>
-
-            <div>
-                <img src="/images/trending/t-5.jpg" alt="genre 5" />
-                <p> <a href="#" className='hover:underline'> Creature Of Ancient Dreaming </a> </p>
-            </div>
+            {trendingBooksData.map((data, index) => (
+                <div key={index}>
+                    <img src={data.image} alt={data.alt} />
+                    <p> <a href="#" className='hover:underline'> {data.title} </a> </p>
+                </div>
+            ))}
         </div>
 
     </div>
